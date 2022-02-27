34b98605ac3ec1c2a3ab39872a26760dd2750fc3
mcrsr2021 <mcrsr2021@gmail.com>
2022-02-27 10:07:59 +0530

    Part 9

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
-from dataclasses import fields
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from users.models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -10,3 +10,17 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
